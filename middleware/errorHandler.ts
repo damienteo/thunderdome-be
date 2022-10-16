28e3003f23c8ccb567e27bbea92816f49b56f74e
damienteo <damien.teo.ks@gmail.com>
2022-10-16 20:20:06 +0800

    Add error handler middleware

---
+++ middleware/errorHandler.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+
+interface IError extends Error {
+  statusCode: number;
+}
+
+const errorHandler = (
+  err: IError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || "Server Error" });
+};
+
+module.exports = errorHandler;
