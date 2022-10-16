b60587c62d9ac774e103cfce075e983a959235da
damienteo <damien.teo.ks@gmail.com>
2022-10-16 20:19:20 +0800

    Add Product model

---
+++ models/products.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+import { IProduct } from "../interfaces/product";
+
+interface ProductDoc extends mongoose.Document<IProduct> {}
+
+interface ProductModelInterface extends mongoose.Model<ProductDoc> {
+  build(attr: IProduct): ProductDoc;
+}
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  tokenId: {
+    type: Number,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+});
+
+productSchema.statics.build = (attr: IProduct) => new Product(attr);
+
+const Product = mongoose.model<any, ProductModelInterface>(
+  "Product",
+  productSchema
+);
+
+export { Product };
